7e51f724b3872605ad0d34c349dfb94a8315cdf5
Aakash Shinghal <aakasshi@amdocs.com>
2019-09-18 11:42:50 +0530

    Changed the SWAP CASE PYTHON CODE. Modified the code so that We can directly use the isLower() function without comparing it with boolean comparator

--- Strings/SwaptheCase.py
+++ Strings/SwaptheCase.py
@@ -4,7 +4,7 @@ print(len(string))
 string1 = ""
 
 for item in range(0, len(string)):
-    if string[item].islower() == True:
+    if string[item].islower():
         string1 += string[item].upper()
     else:
         string1 += string[item].lower()
